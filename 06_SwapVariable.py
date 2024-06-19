7844d33e0cdc466e0a483d406d75f384a4327bfb
Shantun Tomar <164908306+Shantun19@users.noreply.github.com>
2024-06-19 23:35:39 +0530

    Create 06_SwapVariable.py

---
+++ 06_SwapVariable.py
@@ -0,0 +1,11 @@
+# swap the variable 
+a = 7
+b = 9
+
+print('the value of a before swapping is {}'.format(a))
+print('the value of b before swapping is {}'.format(b))
+
+a , b = b , a
+
+print('the value of a after swapping is {}'.format(a))
+print('the value of b after swapping is {}'.format(b))
