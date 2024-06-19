9a84a9e6eb0ebee748a0280eecd9e7085998a2f1
Shantun Tomar <164908306+Shantun19@users.noreply.github.com>
2024-06-19 23:46:41 +0530

    Create 08_NumberIsPositiveOrNegative.py

---
+++ 08_NumberIsPositiveOrNegative.py
@@ -0,0 +1,11 @@
+# check weather number is +ve , -ve or 0
+
+# user input 
+n = int(input('Enter a value !!'))
+
+if n > 0:
+    print('Number is positive')
+elif n == 0:
+    print('Number is 0')
+else:
+    print('Number is Negative')
