4356e0afc85e82e1b03c7c574886d1391ae97d8e
Shantun Tomar <164908306+Shantun19@users.noreply.github.com>
2024-06-19 23:51:25 +0530

    Create 09_NumberIsEvenOrOdd.py

---
+++ 09_NumberIsEvenOrOdd.py
@@ -0,0 +1,9 @@
+# check weather number even or odd 
+
+# user input 
+n = int(input('Enter a value !!'))
+
+if (n%2) == 0:
+    print('{} is Even Number'.format(n))
+else :
+    print('{} is a Odd Number'.format(n))
